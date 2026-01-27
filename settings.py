689cdb74731da3da87985be88835516e08465545
arjanbrussee <fr00tl00p>
2026-01-27 20:21:09 +0100

    Update image extension handling in settings.py. Removed duplicate entries from gExifImageExtensions and added a new counter for skipped photos in the Photos library due to unavailability.

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ gOutputPath = "/path/to/output"
 gTempPath = "/path/to/temp"
 gDatabasePath = None  # Database directory path (defaults to gOutputPath if not set)
 gImageExtensions = ["jpg", "jpeg", "png", "tif", "tiff", "gif", "bmp", "heic", "heif", "mov", "mp4", "m4v", "m4a", "m4b", "m4p", "m4v", "m4a", "m4b", "m4p", "cr2", "nef"]
-gExifImageExtensions = ["jpg", "jpeg", "tif", "tiff", "heic", "heif", "mov", "mp4", "m4v", "m4a", "m4b", "m4p", "m4v", "m4a", "m4b", "m4p", "cr2", "nef"]
+gExifImageExtensions = ["jpg", "jpeg", "tif", "tiff", "mp4", "m4v", "m4a", "m4b", "m4p", "m4v", "m4a", "m4b", "m4p", "cr2", "nef"]
 gIgnoreFolders = ["__MACOSX", "Data.noindex", ".Trash", "Caches", "Thumbnails", "com.apple.AddressBook.", "Library/Containers", "Application Support"]
 gDatabase = None
 
@@ -14,3 +14,4 @@ gZipImageCount = 0     # Number of images scanned in ZIP files
 gSkippedBetterCount = 0  # Number of files skipped because better versions exist
 gSkippedDatabaseCount = 0  # Number of files skipped because already in database
 gNonImageFileCount = 0  # Number of non-image files encountered
+gSkippedPhotosLibraryCount = 0  # Number of photos skipped in Photos library because file not available (e.g., iCloud not downloaded)
